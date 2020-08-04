60f0b5e75a4a625ef2a94351f487069bde328847
bartxs <b.soltysik90@gmail.com>
2020-08-04 14:32:33 +0200

    Exercise

---
+++ Bartosz/javaexe.js
@@ -0,0 +1,29 @@
+// 1.
+
+console.log("this is an exercise in console logging");
+
+// 2.
+
+let value = "This is the value"
+console.log(value);
+
+// 3.
+
+let firstName = "Bartosz"
+let lastName = "Soltysik"
+
+console.log(firstName, lastName);
+
+// 4.
+
+let name = "Someone"
+let city = "New York"
+let job = "Actor"
+let age = "35"
+let sex = "Male"
+
+console.log(name, city, job, age, sex);
+
+// 5.
+
+console.log(name + " is a " + age + " years old " + job + " from " + city);
