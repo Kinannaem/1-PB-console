e9e91a80efb3d83814587cdbf343c0d9a8e3964a
kinannaem <kinan.berlin@gmail.com>
2020-08-04 15:30:08 +0200

    Create consoleExcercies.js
    Co-Authored-By: Ghassan Aldarwish <ghassanaldarwish@yahoo.com>

---
+++ consoleExcercies.js
@@ -0,0 +1,16 @@
+console.log("this is an exercise in console logging");
+let FirstVariable = "This is the value.";
+
+console.log(FirstVariable);
+let FirstName = "Kinan";
+let LastName = "Naem";
+
+console.log(`My name is ${FirstName} ${LastName}`);
+
+let address = "Berlin,Germany";
+let profession = "Graphic Design & UI/UX Designer";
+
+let hobby = "table tennis and basketball";
+let experince = "Photoshop, Illustrator, Indesign, Html5, CSS3";
+console.log(`About Me : I am ${FirstName} ${LastName}, I lived currently in ${address}.
+ I work as ${profession}, I have experiance in ${experince}, My Hobby is ${hobby} `);
