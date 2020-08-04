bcb72eb5d7da92beee32ca93220f0e1636bd8526
arezoofaraji <arezoo.faraji66@gmail.com>
2020-08-04 14:22:12 +0200

    first ex arezoo

---
+++ arezoo/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+console.log("this is an exercise in console logging");
+let myVariable = "This is the value.";
+let firstName = "arezoo";
+let lastName = "faraji";
+console.log(firstName, lastName);
+let country = "Iran";
+let city = "teheran";
+let age = "30";
+let job = "banking business analyst";
+let language = "Persian,Englisch,Germany";
+console.log(
+  "my name is:" +
+    firstName +
+    ", my Family name is: " +
+    lastName +
+    ", " +
+    "I'm from " +
+    country +
+    "-" +
+    city +
+    ", " +
+    "I'm " +
+    age +
+    " years old" +
+    " I have worked as a " +
+    job +
+    " and speak " +
+    language +
+    "."
+);
