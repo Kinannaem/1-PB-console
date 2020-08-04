c45f012546fa64d39e4fcd2a3f5d706be1852499
frausafiye <s_gulenc@outlook.com>
2020-08-04 14:40:36 +0200

    added solutions

---
+++ exercise-1.js
@@ -0,0 +1,36 @@
+//1
+console.log("This is an exercise in console logging.");
+
+//2
+let myFirstVariable = "This is the value.";
+console.log(myFirstVariable);
+//3
+let firstName = "Safiye";
+let lastName = "Guelenc";
+console.log(firstName + ", " + lastName);
+//4
+let name = "Sara";
+let surname = "Grund";
+let job = "teacher";
+let nationality = "german";
+let familyStatus = "married";
+
+console.log(name);
+console.log(surname);
+console.log(job);
+console.log(nationality);
+console.log(familyStatus);
+//5
+console.log(
+  name +
+    " " +
+    surname +
+    " is" +
+    " a " +
+    familyStatus +
+    " " +
+    nationality +
+    " " +
+    job +
+    "."
+);
